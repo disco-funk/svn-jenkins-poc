8143cb6a6964e83dc9ffdbf5aef72d0648839841
Hudson, Philip <Philip.Hudson@zuhlke.com>
2019-03-21 12:04:04 +0000

    (feat): initialise example-repo with c++ files

--- up.sh
+++ up.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 docker-compose up --build --detach
-docker exec -it svn-jenkins-poc_svn-server_1 svnadmin create new-repo
+docker exec -it svn-jenkins-poc_svn-server_1 svnadmin create example-repo
 
-svn info svn://localhost:3690/new-repo
+svn info svn://localhost:3690/example-repo
+
+rm -rf checkedout
+mkdir checkedout
+cd checkedout
+svn checkout svn://localhost:3690/example-repo
+cp -r ../example-repo/ ./example-repo/
+cd example-repo
+svn commit -m "Initial commit"
+cd ../..
