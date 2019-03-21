6fca4c4082b1d677496aa8784e514802ae77e886
Hudson, Philip <Philip.Hudson@zuhlke.com>
2019-03-21 15:05:02 +0000

    (feat): add makefiles

---
+++ example-repo/module1/something.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Output from Module 1 - something";
+    return 0;
+}
---
+++ example-repo/module2/otherthing.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Output from Module 2 - otherthing";
+    return 0;
+}
