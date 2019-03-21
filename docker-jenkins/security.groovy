97344a9fe73dd09a82835f8f4e070b2f2926987d
Hudson, Philip <Philip.Hudson@zuhlke.com>
2019-03-21 12:45:32 +0000

    (build): set admin password

---
+++ docker-jenkins/security.groovy
@@ -0,0 +1,14 @@
+#!groovy
+
+import hudson.security.*
+import jenkins.model.*
+
+def instance = Jenkins.getInstance()
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin", "admin")
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+instance.save()
