73b8f9c411cc6fa0dae319529f1e29bdae2263f8
Hudson, Philip <Philip.Hudson@zuhlke.com>
2019-03-22 13:39:13 +0000

    (refactor): Change to scripted pipeline

--- docker-jenkins/Jenkinsfile.groovy
+++ docker-jenkins/Jenkinsfile.groovy
@@ -1,30 +1,40 @@
 #!groovy
 
-pipeline {
-    agent any
-    stages {
-        stage('Checkout') {
-            steps {
-                checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'svn://svn-server/example-repo']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
-            }
-        }
-        stage('Build') {
-            parallel {
-                stage('Make Module 1') {
-                    steps {
-                        dir("module1") {
-                            sh 'make'
-                        }
-                    }
-                }
-                stage('Make Module 2') {
-                    steps {
-                        dir("module2") {
-                            sh 'make'
-                        }
-                    }
-                }
-            }
+node {
+    stage('Checkout') {
+        checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'svn://svn-server/example-repo']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+    }
+    stage('Getlist of changes') {
+        print getModules()
+
+        print getChangeSetDirectories()
+    }
+}
+
+List<String> getChangeSetDirectories() {
+    def changeLogSets = currentBuild.rawBuild.changeSets
+    List affectedPaths = []
+    List subdirectories = []
+
+    for (int i = 0; i < changeLogSets.size(); i++) {
+        def entries = changeLogSets[i].items
+        for (int j = 0; j < entries.length; j++) {
+            def entry = entries[j]
+            affectedPaths = affectedPaths.plus(entry.getAffectedPaths())
         }
     }
+
+    for (int i = 0; i < affectedPaths.size(); i++) {
+        def split = affectedPaths[i].split("/")
+        subdirectories.push(split[1])
+    }
+
+    return subdirectories
+}
+
+List<String> getModules() {
+    return sh(
+            script: 'ls -d *',
+            returnStdout: true
+    ).split("\n").findAll { it.indexOf('@') == -1 }
 }
